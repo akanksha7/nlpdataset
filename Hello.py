a233b1fe6a2fa9655bd39d842ee474412362fdc2
Akanksha Singh <akanksha.singh0710@gmail.com>
2024-02-08 22:53:02 +0000

    removing main menu

--- Hello.py
+++ Hello.py
@@ -11,11 +11,18 @@ import pathlib
 
 code_dir = pathlib.Path(__file__).parent.resolve()
 
-main_menu = """
-#MainMenu {
-  visibility: hidden;
-}
-"""
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # Constants
 countries = ["Algeria", "Bahrain", "Egypt", "Iraq", "Jordan", "Kuwait", "Libya", "Morocco", "Mauritania", "Oman",
